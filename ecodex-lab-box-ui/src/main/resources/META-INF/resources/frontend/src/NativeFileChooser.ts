39fcb2cc3c282717a8d0e43b70781d929cc229d8
weinek <bruno.weinek@extern.brz.gv.at>
2021-07-29 11:58:26 +0200

    adds custom example component, slows down compilation a lot

---
+++ ecodex-lab-box-ui/src/main/resources/META-INF/resources/frontend/src/NativeFileChooser.ts
@@ -0,0 +1,16 @@
+// import {LitElement, html} from 'lit-element';
+// import '@vaadin/vaadin-text-field/vaadin-text-field.js';
+// import '@vaadin/vaadin-button/vaadin-button.js';
+// import '@axa-ch/input-text';
+//
+//
+// class NativeFileChooser extends LitElement {
+//
+//     render() {
+//         return html`
+//             <div id="content"></div>
+//             <input type="file" name="files" id="files" webkitdirectory directory>`;
+//     }
+// }
+//
+// customElements.define('native-file-chooser', NativeFileChooser);
