6dc3f7c653df41ea76428951f15285dc5f72b472
riederb <bernhard.rieder@brz.gv.at>
2021-08-05 13:18:23 +0200

    added batch scripts to build lab-box in an external environment

---
+++ ecodex-lab-box/build_lab_env.bat
@@ -0,0 +1,77 @@
+
+@ECHO OFF 
+SETLOCAL
+
+set HOME_DIR=%cd%
+
+GOTO ParseParams
+
+:ParseParams
+    If "%~1"=="" GOTO SetMavenHome
+
+    SET TempVar=%~1
+    IF "%TempVar:~0,7%"=="lab.id:" ( 
+		echo.
+		echo lab.id Parameter found
+        SET LAB_ID=%TempVar:~7,250%
+    )
+
+    SHIFT
+
+GOTO ParseParams
+
+:SetMavenHome
+set "MAVEN_HOME=%cd%\apache-maven-3.8.1\bin\"
+
+If "%LAB_ID"=="" goto usage
+
+echo.
+echo LAB_ID is %LAB_ID%
+
+set "MAVEN_CMD=%MAVEN_HOME%mvn.cmd "
+
+echo.
+echo MAVEN_CMD is %MAVEN_CMD%
+
+rem set "MAVEN_ARGS=install -Dlab.id=%LAB_ID%"
+
+rem echo MAVEN_ARGS is %MAVEN_ARGS%
+
+
+
+echo.
+echo ##################### calling %MAVEN_CMD% install -Dlab.id=%LAB_ID% #####################
+echo.
+
+call "%MAVEN_CMD%" install -Dlab.id=%LAB_ID%
+
+echo.
+echo ##################### labenv%LAB_ID% built #####################
+echo.
+
+echo.
+echo ##################### Setup of domibus-gateway database #####################
+echo.
+call %cd%\labenv%LAB_ID%\domibus-gateway\setup\setupdb.bat
+
+rem call %cd%\labenv%LAB_ID%\domibus-gateway\setup\setuppmodes.bat
+
+goto end
+
+:usage
+echo.
+echo ##################### Usage of build_lab_env script #####################
+echo.
+echo This script is to create a new e-Codex Lab Box with a certain environment id .
+echo Parameters are to be entered with name:value pair.
+echo.
+echo Parameters:
+echo.
+echo lab.id			specifies the numeric value of the lab environment to be created. Must be a value between 01 and 10
+echo.
+echo Example: build_lab_env.bat lab.id=01
+echo.
+echo ########################################################################
+
+
+:end
