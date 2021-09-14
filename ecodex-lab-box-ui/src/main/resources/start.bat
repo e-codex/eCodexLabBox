34167c8a15a1dfa70b279c57d3122f2ab861dd1e
riederb <bernhard.rieder@brz.gv.at>
2021-09-14 07:58:53 +0200

    structured distribution package

---
+++ ecodex-lab-box-ui/src/main/resources/start.bat
@@ -0,0 +1,32 @@
+@echo off
+rem This is the automatically built startup script for the ecodex-lab-box-ui.
+rem To be able to run the JAVA_HOME system environment variable must be set properly.
+
+if exist "%JAVA_HOME%" goto okJava
+call setenv.bat
+if exist "%JAVA_HOME%" goto okJava
+echo The JAVA_HOME environment variable is not defined correctly
+echo This environment variable is needed to run this program
+goto end
+:okJava
+
+rem set "PATH=%PATH%;%JAVA_HOME%\bin"
+
+set "CURRENT_DIR=%cd%"
+
+set "CLASSPATH=%CURRENT_DIR%\ui\*"
+echo %CLASSPATH%
+
+REM set "LIB_FOLDER=%CURRENT_DIR%\ui\"
+
+
+
+
+title "ecodex-lab-box-ui"
+
+
+@echo on
+"%JAVA_HOME%\bin\java" -D"loader.path=./ui" -cp "%CLASSPATH%" "org.springframework.boot.loader.PropertiesLauncher"
+
+:end
+
