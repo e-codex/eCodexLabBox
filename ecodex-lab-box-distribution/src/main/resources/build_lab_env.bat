977db8f49dd876f5a0c8d9bc3cd538a4fd0401ca
riederb <bernhard.rieder@brz.gv.at>
2021-09-22 09:29:40 +0200

    added proxy handling in build_lab_env.bat

--- ecodex-lab-box-distribution/src/main/resources/build_lab_env.bat
+++ ecodex-lab-box-distribution/src/main/resources/build_lab_env.bat
@@ -15,6 +15,26 @@ GOTO ParseParams
 		echo lab.id Parameter found
         SET LAB_ID=%TempVar:~7,250%
     )
+    IF "%TempVar:~0,16%"=="http.proxy.host:" ( 
+		echo.
+		echo http.proxy.host Parameter found
+        SET HTTP_PROXY_HOST=%TempVar:~16,250%
+    )
+    IF "%TempVar:~0,16%"=="http.proxy.port:" ( 
+		echo.
+		echo http.proxy.port Parameter found
+        SET HTTP_PROXY_PORT=%TempVar:~16,250%
+    )
+	IF "%TempVar:~0,17%"=="https.proxy.host:" ( 
+		echo.
+		echo https.proxy.host Parameter found
+        SET HTTPS_PROXY_HOST=%TempVar:~17,250%
+    )
+    IF "%TempVar:~0,17%"=="https.proxy.port:" ( 
+		echo.
+		echo https.proxy.port Parameter found
+        SET HTTPS_PROXY_PORT=%TempVar:~17,250%
+    )
 
     SHIFT
 
@@ -34,17 +54,24 @@ set "MAVEN_CMD=%MAVEN_HOME%mvn.cmd "
 echo.
 echo MAVEN_CMD is %MAVEN_CMD%
 
-rem set "MAVEN_ARGS=install -Dlab.id=%LAB_ID%"
+SET MAVEN_ARGS=-Dlab.id=%LAB_ID%
 
-rem echo MAVEN_ARGS is %MAVEN_ARGS%
+IF DEFINED HTTP_PROXY_HOST (SET MAVEN_ARGS=%MAVEN_ARGS% -Dhttp.proxyHost=%HTTP_PROXY_HOST%)
+IF DEFINED HTTP_PROXY_PORT (SET MAVEN_ARGS=%MAVEN_ARGS% -Dhttp.proxyPort=%HTTP_PROXY_PORT%)
+IF DEFINED HTTPS_PROXY_HOST (SET MAVEN_ARGS=%MAVEN_ARGS% -Dhttps.proxyHost=%HTTPS_PROXY_HOST%)
+IF DEFINED HTTPS_PROXY_PORT (SET MAVEN_ARGS=%MAVEN_ARGS% -Dhttps.proxyPort=%HTTPS_PROXY_PORT%)
+
+rem SET MAVEN_ARGS=%MAVEN_ARGS% -Dhttps.nonProxyHosts=localhost
+
+echo MAVEN_ARGS is %MAVEN_ARGS%
 
 
 
 echo.
-echo ##################### calling %MAVEN_CMD% install -Dlab.id=%LAB_ID% #####################
+echo ##################### calling %MAVEN_CMD% install %MAVEN_ARGS% #####################
 echo.
 
-call "%MAVEN_CMD%" install -Dlab.id=%LAB_ID%
+call "%MAVEN_CMD%" install %MAVEN_ARGS%
 
 echo.
 echo ##################### labenv%LAB_ID% built #####################
@@ -73,9 +100,14 @@ echo Parameters are to be entered with name:value pair.
 echo.
 echo Parameters:
 echo.
-echo lab.id			specifies the numeric value of the lab environment to be created. Must be a value between 01 and 10
+echo lab.id			   specifies the numeric value of the lab environment to be created. Must be a value between 01 and 09
+echo http.proxy.host   specifies what proxy host should be used to connect to the outside world.
+echo http.proxy.port   specifies what proxy port should be used to connect to the outside world.
+echo https.proxy.host  specifies what proxy host should be used to connect to the outside world via https.
+echo https.proxy.port  specifies what proxy port should be used to connect to the outside world via https.
 echo.
-echo Example: build_lab_env.bat lab.id:01
+echo Example:                  build_lab_env.bat lab.id:01
+echo Example with https proxy: build_lab_env.bat lab.id:01 https.proxy.host:127.0.0.1 https.proxy.port:80
 echo.
 echo ########################################################################
 
