102a0ca39fb999c49125f77e4f628b344cd8b23a
weinek <bruno.weinek@extern.brz.gv.at>
2021-09-30 12:54:54 +0200

    adds startup event that opens the url of the app in the default browser

--- ecodex-lab-box-ui/src/main/java/eu/ecodex/labbox/ui/service/RunAfterStartupService.java
+++ ecodex-lab-box-ui/src/main/java/eu/ecodex/labbox/ui/service/RunAfterStartupService.java
@@ -1,17 +1,26 @@
 package eu.ecodex.labbox.ui.service;
 
 import eu.ecodex.labbox.ui.controller.DirectoryController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
+
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Service
 public class RunAfterStartupService {
 
     final DirectoryController directoryController;
+    final String port;
 
-    public RunAfterStartupService(DirectoryController directoryController) {
+    public RunAfterStartupService(DirectoryController directoryController,  @Value("${server.port}") String port) {
         this.directoryController = directoryController;
+        this.port = port;
     }
 
     // ContextRefreshedEvent event -> when context is completely initialized
@@ -21,5 +30,18 @@ public class RunAfterStartupService {
         directoryController.startMonitoring();
         directoryController.searchForMaven();
         directoryController.searchForLab();
+    }
+
+    @EventListener
+    public void openBrowser(ApplicationReadyEvent event) {
+        String url = "http://localhost:" + port;
+        if (Desktop.isDesktopSupported()) {
+            Desktop desktop = Desktop.getDesktop();
+            try {
+                desktop.browse(new URI(url));
+            } catch (IOException | URISyntaxException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
