7a837367f2ca9c3f995c41fe60becbc438f9047c
riederb <bernhard.rieder@brz.gv.at>
2021-10-11 07:35:11 +0200

    embedding asciidoc tutorial into vaadin HelpView

--- ecodex-lab-box-ui/src/main/java/eu/ecodex/labbox/ui/view/help/HelpView.java
+++ ecodex-lab-box-ui/src/main/java/eu/ecodex/labbox/ui/view/help/HelpView.java
@@ -1,17 +1,12 @@
 package eu.ecodex.labbox.ui.view.help;
 
-import com.vaadin.flow.component.html.IFrame;
 import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.html.IFrame;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.tabs.TabVariant;
-import com.vaadin.flow.router.AfterNavigationEvent;
-import com.vaadin.flow.router.AfterNavigationObserver;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.spring.annotation.UIScope;
 import eu.ecodex.labbox.ui.configuration.TabMetadata;
-import eu.ecodex.labbox.ui.controller.UpdateFrontendController;
-import eu.ecodex.labbox.ui.view.BaseViewVertical;
-import eu.ecodex.labbox.ui.view.labenvironment.NotificationReceiver;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -20,21 +15,15 @@ import org.springframework.stereotype.Component;
 @Route(value = HelpView.ROUTE, layout = HelpLayout.class)
 @Order(1)
 @TabMetadata(title = "Tutorial", icon = VaadinIcon.LIGHTBULB, themeVariant = TabVariant.LUMO_ICON_ON_TOP, tabGroup = HelpLayout.TAB_GROUP_NAME)
-public class HelpView extends BaseViewVertical implements AfterNavigationObserver, NotificationReceiver {
+public class HelpView extends VerticalLayout {
 
     public static final String ROUTE = "tutorial";
 
-    public HelpView(UpdateFrontendController updateFrontendController) {
-        super(updateFrontendController);
+    public HelpView() {
 
         IFrame tutorial = new IFrame("html/tutorial.html");
         tutorial.setWidth("100%");
 
         add(tutorial);
-    }
-
-    @Override
-    public void afterNavigation(AfterNavigationEvent event) {
-        updateAppStateNotification();
     }
 }
